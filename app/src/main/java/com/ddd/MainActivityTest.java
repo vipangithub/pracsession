457750aa381d3b2a2e3eee1b575ff41a477b61c9
Vipan <vipansharmait@gmail.com>
2018-08-26 13:10:46 +0530

    initial commit

---
+++ app/src/main/java/com/ddd/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.ddd;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
